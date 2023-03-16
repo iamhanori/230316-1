a6218cdfd0fced3ea5f064c241d2748791ebdd20
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-16 15:41:28 +0900

    구분선..

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -11,20 +11,28 @@ public class Main {
         System.out.println("obj1 : "+obj1);
         obj1.prData();
 
+        System.out.println("----------------------------------");
+
         // int
         TestBean1 obj2 = ctx.getBean("t2", TestBean1.class);
         System.out.println("obj2 : "+obj2);
         obj2.prData();
+
+        System.out.println("----------------------------------");
 
         // double
         TestBean1 obj3 = ctx.getBean("t3", TestBean1.class);
         System.out.println("obj3 : "+obj3);
         obj3.prData();
 
+        System.out.println("----------------------------------");
+
         // String
         TestBean1 obj4 = ctx.getBean("t4", TestBean1.class);
         System.out.println("obj4 : "+obj4);
         obj4.prData();
+
+        System.out.println("----------------------------------");
 
         ctx.close();
     }
