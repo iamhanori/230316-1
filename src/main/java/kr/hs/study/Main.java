28138eaf4121a33d03c5c748f195b97ecc1535b6
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-16 15:08:57 +0900

    constructor-arg 사용하여 값 넣어 출력

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -3,12 +3,20 @@ package kr.hs.study;
 import kr.hs.study.beans.TestBean1;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
 
-        TestBean1 obj = ctx.getBean("t1", TestBean1.class);
-        System.out.println("obj : "+obj);
+        TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
+        System.out.println("obj1 : "+obj1);
+        obj1.prData();
 
+        TestBean1 obj2 = ctx.getBean("t2", TestBean1.class);
+        System.out.println("obj2 : "+obj2);
+        obj2.prData();
+
+        ctx.close();
     }
 }
--- src/main/java/kr/hs/study/beans/TestBean1.java
+++ src/main/java/kr/hs/study/beans/TestBean1.java
@@ -13,6 +13,13 @@ public class TestBean1 {
 
     }
 
+    public TestBean1(int data1) {
+        System.out.println("TestBean1의 int형 데이터 갖는 생성자");
+        this.data1 = data1;
+        this.data2 = 1.1;
+        this.data3 = "spring1";
+    }
+
     // prData method
     public void prData() {
         System.out.println("data1 : " + data1);
