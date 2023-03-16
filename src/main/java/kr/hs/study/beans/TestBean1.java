9e6b3111c7f6b0c3e72ef5e6186bb71fe8937996
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-16 14:58:34 +0900

    생성자 메서드 생성

--- src/main/java/kr/hs/study/beans/TestBean1.java
+++ src/main/java/kr/hs/study/beans/TestBean1.java
@@ -1,5 +1,22 @@
 package kr.hs.study.beans;
 
 public class TestBean1 {
-    TestBean1(){ }
+    private int data1;
+    private double data2;
+    private String data3;
+
+    // 생성자
+    public TestBean1() {
+        this.data1 = 0;
+        this.data2 = 0.0;
+        this.data3 = "spring0";
+
+    }
+
+    // prData method
+    public void prData() {
+        System.out.println("data1 : " + data1);
+        System.out.println("data2 : " + data2);
+        System.out.println("data3 : " + data3);
+    }
 }
